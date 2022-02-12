d50dd364a8c3e8a1855622a382ab7c5d71831d92
iAbhishekPatil <abhishek.patil@joflee.com>
2022-02-12 18:59:01 +0530

    Auth routing added

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../auth/application/auth_notifier.dart';
+import '../../auth/shared/providers.dart';
 import '../routes/app_router.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider<void>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+});
+
+class AppWidget extends ConsumerWidget {
   AppWidget({Key? key}) : super(key: key);
 
   final appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AuthState>(authNotifierProvider, (previous, next) {
+      next.maybeMap(
+        orElse: () {},
+        authenticated: (_) {
+          appRouter.pushAndPopUntil(
+            const StarredReposRoute(),
+            predicate: (route) => false,
+          );
+        },
+        unAuthenticated: (_) {
+          appRouter.pushAndPopUntil(
+            const SignInRoute(),
+            predicate: (route) => false,
+          );
+        },
+      );
+    });
+    ref.listen(initializationProvider, (previous, next) {});
     return MaterialApp.router(
       title: 'Repo Viewer',
       routerDelegate: appRouter.delegate(),
